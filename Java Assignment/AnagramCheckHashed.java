1bcfbc0ba85c51f8ecc4cf6b934d8410ece45a25
karthikpohane <kartikpohane0612@gmail.com>
2025-03-08 13:38:27 +0530

    Java AnagramCheck Code Added

---
+++ Java Assignment/AnagramCheckHashed.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AnagramCheckHashed {
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert to lowercase for case insensitivity
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths are not equal, they cannot be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Use a HashMap to count character frequencies
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : str1.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : str2.toCharArray()) {
+            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first string: ");
+        String str1 = scanner.nextLine();
+        
+        System.out.print("Enter second string: ");
+        String str2 = scanner.nextLine();
+        
+        System.out.println("Output: " + areAnagrams(str1, str2));
+        
+        scanner.close();
+    }
+}
